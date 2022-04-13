098d442f4c945f3f9197a4c0f1ab13deecb44378
Михаил <@DESKTOP-3P2543V>
2022-04-13 16:06:14 +0300

    Добавьте файлы проекта.

---
+++ VASEK5-6/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace VASEK5_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBoxY = new System.Windows.Forms.ComboBox();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxHx = new System.Windows.Forms.TextBox();
+            this.textBoxNx = new System.Windows.Forms.TextBox();
+            this.textBoxX0 = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.kolvoN = new System.Windows.Forms.Button();
+            this.buttonY = new System.Windows.Forms.Button();
+            this.buttonX = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelN = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(204, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(426, 115);
+            this.textBox1.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(788, 364);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(780, 336);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.comboBoxY);
+            this.tabPage2.Controls.Add(this.Calculate);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.textBoxHx);
+            this.tabPage2.Controls.Add(this.textBoxNx);
+            this.tabPage2.Controls.Add(this.textBoxX0);
+            this.tabPage2.Controls.Add(this.textBoxY);
+            this.tabPage2.Controls.Add(this.kolvoN);
+            this.tabPage2.Controls.Add(this.buttonY);
+            this.tabPage2.Controls.Add(this.buttonX);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.labelN);
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(780, 336);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxY
+            // 
+            this.comboBoxY.FormattingEnabled = true;
+            this.comboBoxY.Location = new System.Drawing.Point(8, 195);
+            this.comboBoxY.Name = "comboBoxY";
+            this.comboBoxY.Size = new System.Drawing.Size(121, 23);
+            this.comboBoxY.TabIndex = 12;
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(239, 307);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(75, 23);
+            this.Calculate.TabIndex = 11;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 15);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Hx";
+            // 
+            // textBoxHx
+            // 
+            this.textBoxHx.Location = new System.Drawing.Point(114, 107);
+            this.textBoxHx.Name = "textBoxHx";
+            this.textBoxHx.Size = new System.Drawing.Size(100, 23);
+            this.textBoxHx.TabIndex = 9;
+            // 
+            // textBoxNx
+            // 
+            this.textBoxNx.Location = new System.Drawing.Point(8, 63);
+            this.textBoxNx.Name = "textBoxNx";
+            this.textBoxNx.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNx.TabIndex = 8;
+            // 
+            // textBoxX0
+            // 
+            this.textBoxX0.Location = new System.Drawing.Point(8, 107);
+            this.textBoxX0.Name = "textBoxX0";
+            this.textBoxX0.Size = new System.Drawing.Size(100, 23);
+            this.textBoxX0.TabIndex = 7;
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(8, 151);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 23);
+            this.textBoxY.TabIndex = 6;
+            // 
+            // kolvoN
+            // 
+            this.kolvoN.Location = new System.Drawing.Point(114, 63);
+            this.kolvoN.Name = "kolvoN";
+            this.kolvoN.Size = new System.Drawing.Size(129, 23);
+            this.kolvoN.TabIndex = 5;
+            this.kolvoN.Text = "Ввести колво точек";
+            this.kolvoN.UseVisualStyleBackColor = true;
+            this.kolvoN.Click += new System.EventHandler(this.kolvoN_Click);
+            // 
+            // buttonY
+            // 
+            this.buttonY.Location = new System.Drawing.Point(114, 151);
+            this.buttonY.Name = "buttonY";
+            this.buttonY.Size = new System.Drawing.Size(94, 23);
+            this.buttonY.TabIndex = 4;
+            this.buttonY.Text = "Ввод y";
+            this.buttonY.UseVisualStyleBackColor = true;
+            this.buttonY.Click += new System.EventHandler(this.buttonY_Click);
+            // 
+            // buttonX
+            // 
+            this.buttonX.Location = new System.Drawing.Point(220, 107);
+            this.buttonX.Name = "buttonX";
+            this.buttonX.Size = new System.Drawing.Size(94, 23);
+            this.buttonX.TabIndex = 3;
+            this.buttonX.Text = "Ввод иксов";
+            this.buttonX.UseVisualStyleBackColor = true;
+            this.buttonX.Click += new System.EventHandler(this.buttonX_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(52, 133);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Y";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "X0";
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(50, 45);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(22, 15);
+            this.labelN.TabIndex = 0;
+            this.labelN.Text = "Nx";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(35, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(48, 15);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Ваши Y";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxHx;
+        private System.Windows.Forms.TextBox textBoxNx;
+        private System.Windows.Forms.TextBox textBoxX0;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.Button kolvoN;
+        private System.Windows.Forms.Button buttonY;
+        private System.Windows.Forms.Button buttonX;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.ComboBox comboBoxY;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ VASEK5-6/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+namespace VASEK5_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double Number = 0;
+            retry:
+            try
+            {
+                Number = Convert.ToDouble(Interaction.InputBox("Vvedi chislo", "CHISLO", "1", -1, -1));
+            }
+            catch(Exception)
+            {
+                textBox1.Text = "Неверный формат данных";
+               DialogResult result = MessageBox.Show("Что делать дальше", "ERROR", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Error);
+                switch (result)
+                {
+                    case DialogResult.Abort:
+                        break;
+                    case DialogResult.Retry:
+                        goto retry;
+                        break;
+                    case DialogResult.Ignore:
+                    Number = 1;
+                        break;
+
+                
+                }
+            }
+
+            MessageBox.Show("Ваше чичсло " + Number);
+        }
+
+        private void kolvoN_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Convert.ToInt32(textBoxNx.Text);
+                if(Convert.ToInt32(textBoxNx.Text) < 0)
+                {
+                    throw new ArgumentNullException("");
+                }
+                textBoxNx.ReadOnly = true;
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("Шаг не может быть отрицательным");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Вы ничего не ввели или ввели строку");
+            }
+        }
+
+        private void buttonX_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Convert.ToDouble(textBoxX0.Text);
+                Convert.ToDouble(textBoxHx.Text);
+                textBoxX0.ReadOnly = true;
+                textBoxHx.ReadOnly = true;
+            }           
+            catch (Exception)
+            {
+                MessageBox.Show("Вы ничего не ввели или ввели строку");
+            }
+
+        }
+
+        private void buttonY_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double a = double.Parse(textBoxY.Text);
+                comboBoxY.Items.Add(textBoxY.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Вы ввели строку или ничего");
+            }
+
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int n;
+                if (textBoxNx.Text == null || textBoxX0.Text == null || textBoxHx.Text == null)
+                {
+                    throw new ArgumentNullException("");
+                }
+                n = Convert.ToInt32(textBoxNx.Text);
+                    for (int i = 1; i <= n; i++)
+                {
+
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                MessageBox.Show("вы не ввели какой-то аргумент");
+            }
+        }
+    }
+
+}
